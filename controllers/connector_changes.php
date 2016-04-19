6b8be217ad990426cea6d66afefed3dd98df942f
Radek <ircdirk@gmail.com>
2016-04-19 16:53:59 +0200

    Update connector_changes.php

--- controllers/connector_changes.php
+++ controllers/connector_changes.php
@@ -1,1 +1,12 @@
-t
+    /* FLASH SESSION TO CODEIGNITER SESSION */
+    private function check_session()
+    {
+        $this->load->library('encrypt');
+        
+        if($this->input->post('session_id') == "") return "";
+        
+        session_start();
+        session_decode(file_get_contents(session_save_path().'/sess_'.$this->encrypt->decode($this->input->post('session_id'))));
+        
+        return $_SESSION[$this->config->item('sess_namespace')];
+    }
