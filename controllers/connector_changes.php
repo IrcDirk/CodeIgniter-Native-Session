442510a81fc99bfe5f230b6c0ea6671285031462
Radek <ircdirk@gmail.com>
2016-04-19 19:59:34 +0200

    Update connector_changes.php

--- controllers/connector_changes.php
+++ controllers/connector_changes.php
@@ -4,7 +4,7 @@
         if($this->input->post('session_id') == "") return "";
         
         session_start();
-        session_decode(file_get_contents(session_save_path().'/sess_'.$this->input->post('session_id')));
+        session_decode(@file_get_contents(session_save_path().'/sess_'.$this->input->post('session_id')));
         
         return $_SESSION[$this->config->item('sess_namespace')];
     }
