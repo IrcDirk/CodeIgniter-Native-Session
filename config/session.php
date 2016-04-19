0ce3d75fea9671befab7170735941de65d3542ac
Radek <ircdirk@gmail.com>
2016-04-19 16:42:32 +0200

    Update session.php

--- config/session.php
+++ config/session.php
@@ -1,7 +1,7 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 // Session namespace
-$config['sess_namespace'] = '';
+$config['sess_namespace'] = 'mediart_cms';
 
 /* End of file session.php */
 /* Location: ./application/config/session.php */
