cfcf3d17c3c3bf20bb1df0408767db314ec12879
Radek <ircdirk@gmail.com>
2016-04-20 07:59:52 +0200

    Update connector_changes.php

--- controllers/connector_changes.php
+++ controllers/connector_changes.php
@@ -1,10 +1,35 @@
-    /* FLASH SESSION TO CODEIGNITER SESSION */
-    private function check_session()
-    {
-        if($this->input->post('session_id') == "") return "";
+    * FLASH SESSION TO CODEIGNITER SESSION */
+	private function check_session()
+	{		
+		if($this->input->post('session_id') == "") return FALSE;
         
+        // LOWER SECURITY
+        ini_set('session.use_trans_sid', 1);
+        ini_set('session.use_strict_mode', 1);
+        ini_set('session.use_cookies', 1);
+        ini_set('session.use_only_cookies', 0);
+        ini_set('session.hash_function', 1);
+        ini_set('session.hash_bits_per_character', 4);
+        
+        // CLOSE ACTIVE SESSION
+        session_write_close();
+        
+        // SET OLD ID AND GET SESSION
+        session_id($this->input->post('session_id'));
         session_start();
-        session_decode(@file_get_contents(session_save_path().'/sess_'.$this->input->post('session_id')));
         
-        return $_SESSION[$this->config->item('sess_namespace')];
-    }
+        $session = $_SESSION[$this->config->item('sess_namespace')];
+        
+        // CLOSE AGAIN
+        session_write_close();
+        
+        // SECURITY
+        ini_set('session.use_trans_sid', 0);
+        ini_set('session.use_strict_mode', 1);
+        ini_set('session.use_cookies', 1);
+        ini_set('session.use_only_cookies', 1);
+        ini_set('session.hash_function', 1);
+        ini_set('session.hash_bits_per_character', 4);
+        
+        return $session;
+	}
